e2acee0daf73eb2ba4edb83a33ea2758093f676a
Adam Myhre <adam@undergalaxie.com>
2018-05-24 17:45:41 -0700

    Added Player movement and Main Camera logic

---
+++ Assets/Project/Scripts/GameCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour {
+
+	public GameObject target;
+	public float verticalOffset;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = new Vector3 (
+			0, 
+			target.transform.position.y + verticalOffset, 
+			transform.position.z
+		);		
+	}
+}
---
+++ Assets/Project/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float horizontalSpeed = 3f;
+	public float verticalSpeed = 1f;
+	public float horizontalLimit = 2.8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Move the plater
+		GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * horizontalSpeed, verticalSpeed);
+		if (transform.position.x > horizontalLimit) {
+			transform.position = new Vector2 (horizontalLimit, transform.position.y);
+		} else if (transform.position.x < -horizontalLimit) {
+			transform.position = new Vector2 (-horizontalLimit, transform.position.y);
+		}
+		
+	}
+}
