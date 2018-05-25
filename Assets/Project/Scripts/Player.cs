b1051d6052d36f1095358faa8f4c02063466291e
Adam Myhre <adam@undergalaxie.com>
2018-05-24 20:05:28 -0700

    Shooting system in place

--- Assets/Project/Scripts/Player.cs
+++ Assets/Project/Scripts/Player.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	public GameObject bulletPrefab;
 	public float horizontalSpeed = 3f;
 	public float verticalSpeed = 1f;
 	public float horizontalLimit = 2.8f;
+	public float bulletSpeed = 2f;
+
+	private bool fired = false;
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +25,21 @@ public class Player : MonoBehaviour {
 			transform.position = new Vector2 (horizontalLimit, transform.position.y);
 		} else if (transform.position.x < -horizontalLimit) {
 			transform.position = new Vector2 (-horizontalLimit, transform.position.y);
+		}
+
+		if (Input.GetAxis ("Fire1") == 1f) {
+			if (!fired) {
+				GameObject bulletInstance = Instantiate (bulletPrefab);
+				bulletInstance.transform.SetParent (transform.parent);
+				bulletInstance.transform.position = transform.position;
+				bulletInstance.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, bulletSpeed);
+
+				Destroy (bulletInstance, 3f);
+
+				fired = true;
+			} 
+		} else {
+			fired = false;
 		}
 		
 	}
